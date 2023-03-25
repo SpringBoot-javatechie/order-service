34c30a23d39672d50a5e3bf7724ec75602fa6fea
Basant kumar Hota <basahota@users.noreply.github.com>
2023-03-25 17:43:44 +0530

    circuit breaker impl

--- src/main/java/com/javatechie/service/OrderService.java
+++ src/main/java/com/javatechie/service/OrderService.java
@@ -7,8 +7,10 @@ import com.javatechie.dto.PaymentDTO;
 import com.javatechie.dto.UserDTO;
 import com.javatechie.entity.Order;
 import com.javatechie.repository.OrderRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +21,7 @@ import java.util.UUID;
 @Service
 public class OrderService {
 
+    public static final String ORDER_SERVICE = "orderService";
     @Autowired
     private OrderRepository repository;
 
@@ -29,7 +32,9 @@ public class OrderService {
     private String topicName;
 
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
+
 
    public String placeAnOrder(Order order){
        //save a copy in order-service DB
@@ -44,20 +49,30 @@ public class OrderService {
        }
        return "Your order with ("+order.getOrderId()+") has been placed ! we will notify once it will confirm";
    }
-
+    @CircuitBreaker(name =ORDER_SERVICE,fallbackMethod = "getOrderDetails")
    public OrderResponseDTO getOrder(String orderId){
        //own DB -> ORDER
-      Order order= repository.findByOrderId(orderId);
-       //PAYMENT-> REST call payment-service
-       PaymentDTO paymentDTO = restTemplate.getForObject("http://PAYMENT-SERVICE/payments/" + orderId, PaymentDTO.class);
-       //user-info-> rest call user-service
-       UserDTO userDTO = restTemplate.getForObject("http://USER-SERVICE/users/" + order.getUserId(), UserDTO.class);
-       return OrderResponseDTO.builder()
-               .order(order)
-               .paymentResponse(paymentDTO)
-               .userInfo(userDTO)
-               .build();
+
+           Order order = repository.findByOrderId(orderId);
+           //PAYMENT-> REST call payment-service
+           PaymentDTO paymentDTO = restTemplate.getForObject("http://PAYMENT-SERVICE/payments/" + orderId, PaymentDTO.class);
+           //user-info-> rest call user-service
+           UserDTO userDTO = restTemplate.getForObject("http://USER-SERVICE/users/" + order.getUserId(), UserDTO.class);
+           return OrderResponseDTO.builder()
+                   .order(order)
+                   .paymentResponse(paymentDTO)
+                   .userInfo(userDTO)
+                   .build();
+
    }
+
+   public OrderResponseDTO getOrderDetails(String orderId,Exception ex){
+       //you can call a DB
+       //you can invoke external api
+       return new OrderResponseDTO("FAILED",null,null,null);
+   }
+
+
 
 
 }
